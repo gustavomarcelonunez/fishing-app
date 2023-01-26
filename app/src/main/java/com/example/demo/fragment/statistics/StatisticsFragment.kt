4c118700bc9f8fa5e6ed12a78b59ec9ef0fc94f3
Gustavo Marcelo Nuñez <gnunez@ing.unp.edu.ar>
2023-01-26 16:21:51 -0300

    Pending: Geofence

--- app/src/main/java/com/example/demo/fragment/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/example/demo/fragment/statistics/StatisticsFragment.kt
@@ -2,7 +2,6 @@ package com.example.demo.fragment.statistics
 
 import android.icu.text.SimpleDateFormat
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,8 +12,6 @@ import com.example.demo.R
 import com.example.demo.databinding.FragmentStatisticsBinding
 import com.example.demo.model.Report
 import com.example.demo.viewModel.ReportViewModel
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 class StatisticsFragment : Fragment() {
@@ -22,6 +19,12 @@ class StatisticsFragment : Fragment() {
     private val reportViewModel: ReportViewModel by navGraphViewModels(R.id.app_navigation)
     private var _binding: FragmentStatisticsBinding? = null
     private val binding get() = _binding!!
+
+    private val currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1
+    private val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+
+    private val sdf = SimpleDateFormat("d/M/yyyy")
+    private val delim = "/"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,67 +42,93 @@ class StatisticsFragment : Fragment() {
     }
 
     private fun fillLabels(reportList: List<Report>) {
-        _binding!!.startActivityTextView.text = reportList.last().date
+        _binding!!.startActivityTextView.text = getFirstDayOfReportList(reportList)
 
-        val currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1
-        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
         var currentMonthReports = 0
-
         var lastSixMonthsReports = 0
-        val sdf = SimpleDateFormat("d/M/yyyy")
 
-        val delim = "/"
-
-        // TODO
-        // Calcular 1, 2 y 3 puesto de capturas
-
-        _binding!!.firstPlaceTextView.text = "pejerrey (10)"
-        _binding!!.secondPlaceTextView.text = "surubi (8)"
-        _binding!!.thirdPlaceTextView.text = "bagre (3)"
-
+        val species = resources.getStringArray(R.array.species)
+        var theSpeciesMap = createMutableMapOf(species)
 
         for (report in reportList) {
             val splitedDate = report.date.split(delim)
-
-            if(splitedDate[1] == currentMonth.toString()) {
+            if(splitedDate[1] == currentMonth.toString() && splitedDate[2] == currentYear.toString()) {
                 currentMonthReports += 1
             }
 
             val actualReportDate: Date = sdf.parse(report.date)
-
-            if(currentMonth <= 6) {
-                val lastYear = currentYear - 1
-                val sixMonthsAgo = 12 - (6 - currentMonth)
-                val auxDate: Date = sdf.parse("1/$sixMonthsAgo/$lastYear")
-
-                val cmpDate = actualReportDate.compareTo(auxDate)
-                when {
-                    cmpDate >= 0 -> {
-                        lastSixMonthsReports += 1
-                    }
-                }
-            }
-            else if (currentMonth > 6) {
-                val sixMonthsAgo = currentMonth - 6
-                val auxDate: Date = sdf.parse("1/$sixMonthsAgo/$currentYear")
-
-                val cmpDate = actualReportDate.compareTo(auxDate)
-                when {
-                    cmpDate >= 0 -> {
-                        lastSixMonthsReports += 1
-                    }
-                }
-            }
-
-
+            lastSixMonthsReports += checkForSixMonthsReport(actualReportDate)
+            theSpeciesMap[report.specie] = (theSpeciesMap[report.specie]?: 0) + 1
         }
-
-        _binding!!.lastMonthTextView.text = currentMonthReports.toString()
+        _binding!!.currentMonthTextView.text = currentMonthReports.toString()
         _binding!!.sixMonthsTextView.text = lastSixMonthsReports.toString()
+
+        //Extract function from here
+        val delimEqual = "="
+
+        var maxBy = theSpeciesMap.maxBy { it.value }
+        var splitedMaxBy = maxBy.toString().split(delimEqual)
+
+        _binding!!.firstPlaceTextView.text = splitedMaxBy[0] + " (" + splitedMaxBy[1] + ")"
+
+        theSpeciesMap.remove(splitedMaxBy[0])
+        maxBy = theSpeciesMap.maxBy { it.value }
+        splitedMaxBy = maxBy.toString().split(delimEqual)
+        _binding!!.secondPlaceTextView.text = splitedMaxBy[0] + " (" + splitedMaxBy[1] + ")"
+
+        theSpeciesMap.remove(splitedMaxBy[0])
+        maxBy = theSpeciesMap.maxBy { it.value }
+        splitedMaxBy = maxBy.toString().split(delimEqual)
+        _binding!!.thirdPlaceTextView.text = splitedMaxBy[0] + " (" + splitedMaxBy[1] + ")"
+        //To here
+
         _binding!!.totalReportsTextView.text = reportList.size.toString()
         _binding!!.lastCaptureTextView.text = reportList.first().specie
     }
 
+    private fun checkForSixMonthsReport(date: Date): Int {
+        var count = 0
+
+        if(currentMonth <= 6) {
+            val lastYear = currentYear - 1
+            val sixMonthsAgo = 12 - (6 - currentMonth)
+            val auxDate: Date = sdf.parse("1/$sixMonthsAgo/$lastYear")
+
+            val cmpDate = date.compareTo(auxDate)
+            when {
+                cmpDate >= 0 -> {
+                    count += 1
+                }
+            }
+        }
+        else if (currentMonth > 6) {
+            val sixMonthsAgo = currentMonth - 6
+            val auxDate: Date = sdf.parse("1/$sixMonthsAgo/$currentYear")
+
+            val cmpDate = date.compareTo(auxDate)
+            when {
+                cmpDate >= 0 -> {
+                    count += 1
+                }
+            }
+        }
+        return count
+    }
+
+    private fun createMutableMapOf(species: Array<String>): MutableMap<String, Int> {
+        var aMutableMapOfSpecies = mutableMapOf<String, Int>()
+        for (specie in species) {
+            aMutableMapOfSpecies[specie] = 0
+        }
+        return aMutableMapOfSpecies
+    }
+
+    private fun getFirstDayOfReportList(reports: List<Report>): CharSequence {
+        if (reports.isEmpty()) {
+            return "No hay reportes cargados"
+        }
+        return reports.last().date
+    }
 
     private fun goToMultipleMaps() {
         findNavController().navigate(R.id.goToMultipleMapsFragmentFromStatisticsAction)
