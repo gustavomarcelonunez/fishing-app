dc103e80f90a77a981aa3e1bf30d022f22c860fc
gustavomarcelonunez <gnunez@ing.unp.edu.ar>
2023-08-13 20:37:28 -0300

    Topics fragment with textviews and buttons

--- app/src/main/java/com/example/demo/fragment/topics/TopicsFragment.kt
+++ app/src/main/java/com/example/demo/fragment/topics/TopicsFragment.kt
@@ -5,56 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.demo.R
+import com.example.demo.databinding.FragmentTopicsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TopicsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TopicsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentTopicsBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // mas variables aqui
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_topics, container, false)
+        _binding = FragmentTopicsBinding.inflate(inflater, container, false)
+
+        _binding!!.goBackButton2.setOnClickListener { goBack() }
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TopicsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TopicsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun goBack() {
+        findNavController().popBackStack()
     }
+
+
 }
